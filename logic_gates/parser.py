45c96e4daf27292c76edd94026d83c2a576a3e02
Tesser3kt <djklepy@gmail.com>
2019-11-14 09:07:49 +0100

    Logic gates app.

---
+++ logic_gates/parser.py
@@ -0,0 +1,13 @@
+""" 
+Unit containing the Parser class, used to convert obtained logical formula
+and convert it into post-fix before constructing a binary tree then used 
+to construct the gate by the 'gate' unit.
+"""
+
+class Parser:
+    
+    """ The parser class used to convert formulae to post-fix and construct
+    binary trees. """
+
+    def __init__(self, formula: str):
+        pass
